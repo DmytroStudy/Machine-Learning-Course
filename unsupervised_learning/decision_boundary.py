d272d2a6bd664e3498d6e8eb671ac13376a83924
DmytroStudy <dmytrostud@gmail.com>
2025-07-22 02:59:49 +0200

    Logistic loss

--- unsupervised_learning/decision_boundary.py
+++ unsupervised_learning/decision_boundary.py
@@ -5,7 +5,7 @@ plt.style.use('../utils/deeplearning.mplstyle')
 
 
 
-# Loading data sets
+# Loading datasets
 X = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
 y = np.array([0, 0, 0, 1, 1, 1]).reshape(-1,1)
 
---
+++ unsupervised_learning/logistic_loss.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils.plt_logistic_loss import  plt_logistic_cost, plt_two_logistic_loss_curves, plt_simple_example
+from utils.plt_logistic_loss import soup_bowl, plt_logistic_squared_error
+plt.style.use('../utils/deeplearning.mplstyle')
+
+# Loading simple dataset for classification
+x_train = np.array([0., 1, 2, 3, 4, 5],dtype=np.longdouble)
+y_train = np.array([0,  0, 0, 1, 1, 1],dtype=np.longdouble)
+plt_simple_example(x_train, y_train) # plot values
+
+# Plot linear regression cost function for logistic regression
+plt_logistic_squared_error(x_train,y_train)
+plt.show()
+
+# Plot logistic regression cost function for logistic regression
+plt_logistic_cost(x_train,y_train)
+plt.show()
